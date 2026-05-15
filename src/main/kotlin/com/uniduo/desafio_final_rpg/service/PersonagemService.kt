8865fac77930904641b01450b9bd6887a0ffdff8
1hjgsth1 <lucasdebrum16@gmail.com>
2026-05-14 21:52:47 -0300

    Correção Codigo

--- src/main/kotlin/com/uniduo/desafio_final_rpg/service/PersonagemService.kt
+++ src/main/kotlin/com/uniduo/desafio_final_rpg/service/PersonagemService.kt
@@ -5,32 +5,33 @@ import com.uniduo.desafio_final_rpg.repository.PersonagemRepository
 import org.springframework.stereotype.Service
 
 @Service
-class PersonagemService(
+class PersonagemService (
     val personagemRepository: PersonagemRepository
-) {
-
-    fun salvar(personagem: Personagem) {
-        if (personagem.nome.isNotBlank() || personagem.nome.isNotEmpty()) {
-            //Se não tenho um Primary Key vou criar uma nova entidade
-            //Se ja tiver um nome igual no banco, somente vai editar
-            //Se tenho um Primary Key somente vou editar
-            personagemRepository.save(personagem)
-        }
+){
+    /**
+     * CRUD do personagem
+     * */
+    //Salvar
+    fun salvar(personagem: Personagem) : Personagem {
+        //Se não tenho um Primary Key vou criar uma nova entidade
+        //se já tiver um nome igual no banco, somente vai editar
+        //Se tenho um Primary Key somente vou editar
+        return personagemRepository.save(personagem)
     }
 
-    fun buscarTodos(): List<Personagem> {
+    //Buscar
+    fun buscarTodos(): List<Personagem>{
         return personagemRepository.findAll()
-
     }
 
-    fun buscarPorNome(nome: String): Personagem {
+    fun buscarPorNome(nome: String) : Personagem{
         return personagemRepository.findById(nome).get()
     }
 
+    //Excluir
     fun excluirPersonagem(nome: String) {
         personagemRepository.deleteById(nome)
     }
 
-    //Passar exemplos da Controladora
-
+    //PASSAR OS EXEMPLOS DA CONTROLADORA
 }
